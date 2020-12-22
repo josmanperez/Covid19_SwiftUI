d9e1e1281eb647efe7b8d6be7cbafd3b9fa91f50
Josman Pérez <josman.perez@mongodb.com>
2020-12-22 09:45:44 +0100

    Initial Commit

---
+++ Covid19Stats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19Stats
+//
+//  Created by Josman Pedro Pérez Expósito on 22/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid19Stats/Covid19StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19StatsApp.swift
+//  Covid19Stats
+//
+//  Created by Josman Pedro Pérez Expósito on 22/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Covid19StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
